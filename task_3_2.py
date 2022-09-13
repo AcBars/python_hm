283e0af4c92e92b6e8ef7c2c1da0c7a7bf0403ce
Artem Mikhailov <mobail_2003@bk.ru>
2022-09-13 15:12:46 +0300

    add task_3_2 working

--- task_3_2.py
+++ task_3_2.py
@@ -5,3 +5,22 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+
+
+from my_functions import rnd_array
+
+def product_numbers(arr):
+    """This is a function that will find the product of pairs of numbers in the list."""
+    arr_result = []
+    for i in range(0, (len(arr) // 2)):
+        if i < len(arr) // 2:
+            arr_result.append(arr[i] * arr[len(arr) - i - 1])
+    if len(arr) % 2 > 0:
+        arr_result.append(arr[len(arr) // 2] ** 2)
+    return arr_result
+
+len_list = int(input('Введите длину списка чисел: '))
+initial_list = rnd_array(len_list)
+
+print(f'{initial_list} => {product_numbers(initial_list)}')
