96d3255836d928d9b0796e9e898e1bbfc345c2d6
Artem_notebook <mobail_2003@bk.ru>
2022-10-27 00:57:24 +0700

    task_5_1 work v.9

--- task_5_1.py
+++ task_5_1.py
@@ -10,41 +10,78 @@
 from random import randint
 from my_functions import check_numberint
 
+def us_progres(step):
+    """Request the user's progress (Запрос хода у пользователя)."""
+    print('Ваш ход!')
+    you_step = check_numberint(input('Введите число конфет которое вы возмете со стола: '))
+    while not(you_step > 0 and you_step <= step):
+        print(f'Вы играете не честно! Договорились брать конфет нне больше {step}')
+        you_step = check_numberint(input('Введите число конфет которое вы возмете со стола: '))
+    return you_step
+
 while True :
     answer = input('Привет! Сыграем в игру конфеты?\n')
     answer = answer.lower()
     if answer == 'да':
         count = check_numberint(input('Введите число (от 300 до 3 000) конфет на столе: '))
-        while not(count >= 300 and count =< 300=):
+        while not(count >= 300 and count <= 3000):
             print('Вы ввели число меньше 300 или больше 3000!')
             count = check_numberint(input('Введите число (от 300 до 3 000) конфет на столе: '))
         step = check_numberint(input('Введите максимальное число (от 3 до 30) конфет которое можно взять за один ход: '))
-        if not(step > 3 and step <30):
+        while not(step >= 3 and step <=30):
             print('Вы ввели число меньше 3 или больше 30!')
             step = check_numberint(input('Введите максимальное число (от 3 до 30) конфет которое можно взять за один ход: '))
         print('Делаем волшебство определяем кто будет ходить!\n...\n3\n2\n1\n0')
         temp = randint(0,1)
         if temp == 1:
-            print('Тебе повезло!\nПерввый ход ваш!')
-        else:
+            print('Сегодня вам повезло!\nПерввый ход ваш!')
             while count > 0:
-                print('Сегодня дача на моей стороне первый хожу я!')
-                if count % step >= 3:
-                    my_step = count % step
-                    print('Мой ход! Я беру со стола ' + my_step)
+                count = count - us_progres(step)
+                if count == 0:
+                    print('Вы выйграли!')
+                    break
+                print(f'На столе осталось {count} конфет')
+                if count % (step + 1) >= 1:
+                    my_step = count % (step + 1)
+                    print(f'Мой ход! Я беру со стола {my_step} конфет')
                     count = count - my_step
-                    print('На столе осталось ' + count - count % step + ' конфет')
-                    print('Ваш ход!')
-                    you_step = check_numberint(input('Введите число конфет которое вы возмете со стола: '))
-                    if not(you_step >= 3 and you_step <= 30):
-                        print('Вы играете не честно! Договорились брать конфет не меньше 3 и не больше 30')
-                        you_step = check_numberint(input('Введите число конфет которое вы возмете со стола: '))
-                    else:
-                        count = count - you_step
+                    if count == 0:
+                        print('Я выйграл!')
+                        break
+                    print(f'На столе осталось {count} конфет')
                 else:
                     my_step = step
-                    
-                        
+                    print(f'Мой ход! Я беру со стола {my_step} конфет')
+                    count = count - my_step
+                    if count == 0:
+                        print('Я выйграл!')
+                        break
+                    print(f'На столе осталось {count} конфет')
+        else:
+            print('Сегодня удача на моей стороне первый хожу я!')
+            while count > 0:
+                if count % (step + 1) >= 1:
+                    my_step = count % (step + 1)
+                    print(f'Мой ход! Я беру со стола {my_step} конфет')
+                    count = count - my_step
+                    if count == 0:
+                        print('Я выйграл!')
+                        break
+                    print(f'На столе осталось {count} конфет')
+                else:
+                    my_step = step
+                    print(f'Мой ход! Я беру со стола {my_step} конфет')
+                    count = count - my_step
+                    if count == 0:
+                        print('Я выйграл!')
+                        break
+                    print(f'На столе осталось {count} конфет')
+                count = count - us_progres(step)
+                if count == 0:
+                    print('Вы выйграли!')
+                    break
+                print(f'На столе осталось {count} конфет')
+        break                     
     else:
         print('Очень жаль! Надеюсь в следующий раз мы с играем!')
         break
