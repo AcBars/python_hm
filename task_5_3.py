545256fa6a0e9f08deb6c0445d5404631934e3b2
Artem_notebook <mobail_2003@bk.ru>
2022-10-28 19:28:21 +0700

    task_5_3 work v.11

--- task_5_3.py
+++ task_5_3.py
@@ -1,1 +1,16 @@
 # задача 3. Напишите программу, удаляющую из текста все слова, содержащие "абв". Функции FIND и COUNT юзать нельзя.
+
+from codecs import utf_8_encode
+from encodings import utf_8
+import re
+
+
+with open('python_hm/task_in_5_3.txt', 'r', encoding = 'utf_8') as f:
+    s = f.read()
+
+reg = r'[А-Яа-я]*абв[А-Яа-я]*|абв[А-Яа-я]*|[А-Яа-я]*абв'
+
+text = re.sub(reg, '', s)
+with open('python_hm/task_ex_5_3.txt', 'w', encoding = 'utf_8') as f:
+    f.write(text)
+print(text)
