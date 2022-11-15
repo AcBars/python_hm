9863282362c9c4d1dcccdb7f595b0c29d84cafde
Artem_notebook <mobail_2003@bk.ru>
2022-11-15 13:38:16 +0700

    task_6_2 working v.17

--- task_6_2.py
+++ task_6_2.py
@@ -16,53 +16,41 @@
 
 #     (1+2)*3 => 9;
 
+import re
 
-def chek_sign(con):
-    if i:= con.find('+') >= 0:
-        con_1 = con[0:i]
-        con_2 = con[i + 1:]
-        result = plus(con_1, con_2)
-    elif i := con.find('-') >= 0:
-        con_1 = con[0:i]
-        con_2 = con[i + 1:]
-        result = minus(con_1, con_2)
-    elif i:= con.find('*') >= 0:
-        con_1 = con[0:i]
-        con_2 = con[i + 1:]
-        result = mult(con_1, con_2)
-    elif i:= con.find('/') >= 0:
-        con_1 = con[0:i]
-        con_2 = con[i + 1:]
-        result = dif(con_1, con_2)
-    else:
-        result = float(con)
-    
-    return result
+con = input('Введите выражение: ')
 
-def mult(mult_1, mult_2):
-    m_con_1 = chek_sign(mult_1)
-    m_con_2 = chek_sign(mult_2)
-    mult_result = float(m_con_1) * float(m_con_2)
-    return mult_result
-
-def dif(dif_1, dif_2):
-    d_con_1 = chek_sign(dif_1)
-    d_con_2 = chek_sign(dif_2)
-    dif_result = float(d_con_1) / float(d_con_2)
-    return dif_result
-
-def plus(plus_1, plus_2):
-    p_con_1 = chek_sign(plus_1)
-    p_con_2 = chek_sign(plus_2)
-    plus_result = float(p_con_1) + float(p_con_2)
-    return plus_result
-
-def minus(minus_1, minus_2):
-    mi_con_1 = chek_sign(minus_1)
-    mi_con_2 = chek_sign(minus_2)
-    minus_result = float(mi_con_1) - float(mi_con_2)
-    return minus_result
-
-con = input('Введите условие: ')
-total_res = chek_sign(con)
-print(f'{con}={total_res}')
+while con.find('*') > 0:
+    reg = r"\d+\*\d+\.\d+|\d+\*\d+|\d+\.\d+\*\d+\.\d+|\d+\.\d+\*\d+"
+    mult = re.search(reg, con)
+    mult = mult.group()
+    mult_arr = mult.split('*')
+    temp = str(float(mult_arr[0]) * float(mult_arr[1]))
+    con = re.sub(reg, temp, con, count=1)
+while con.find('/') > 0:
+    reg = r"\d+\/\d+\.\d+|\d+\/\d+|\d+\.\d+\/\d+\.\d+|\d+\.\d+\/\d+"
+    mult = re.search(reg, con)
+    mult = mult.group()
+    mult_arr = mult.split('/')
+    temp = str(float(mult_arr[0]) / float(mult_arr[1]))
+    con = re.sub(reg, temp, con, count=1)
+while con.find('+') > 0:
+    reg = r"\d+\+\d+\.\d+|\d+\+\d+|\d+\.\d+\+\d+\.\d+|\d+\.\d+\+\d+"
+    mult = re.search(reg, con)
+    mult = mult.group()
+    mult_arr = mult.split('+')
+    temp = str(float(mult_arr[0]) + float(mult_arr[1]))
+    con = re.sub(reg, temp, con, count=1)
+if con.find('-') == 0:
+    mult_arr = con.split('-')
+    total = -1 * float(mult_arr[1])
+    for i in range(2, len(mult_arr)):
+        total -= float(mult_arr[i])
+        con = total
+elif con.find('-') > 0:
+    mult_arr = con.split('-')
+    total = float(mult_arr[0])
+    for i in range(1, len(mult_arr)):
+        total -= float(mult_arr[i])
+        con = total
+print(con)
